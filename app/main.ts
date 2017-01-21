3728364d98366fb523003d63a5356bd746e38dd1
Rajesh Kumar <rajinfoskill@gmail.com>
2017-01-21 18:27:36 +0530

    Adding bootstrap ts file

---
+++ app/main.ts
@@ -0,0 +1,10 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { enableProdMode } from '@angular/core';
+
+import { AppModule } from './app.module';
+
+enableProdMode();
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+    .then(success => console.log(`Bootstrap success`))
+    .catch(error => console.log(error));
